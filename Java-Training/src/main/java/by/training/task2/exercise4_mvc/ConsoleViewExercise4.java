60fee69dddde3dae6ae077e8c70c0dcb24533efb
looost <m.naumenko94@gmail.com>
2019-12-14 22:15:07 +0300

    adding MVC task2

--- Java-Training/src/main/java/by/training/task2/exercise4_mvc/ConsoleViewExercise4.java
+++ Java-Training/src/main/java/by/training/task2/exercise4_mvc/ConsoleViewExercise4.java
@@ -3,19 +3,6 @@ package by.training.task2.exercise4_mvc;
 public class ConsoleViewExercise4 implements ViewExercise4 {
     @Override
     public void showTask() {
-        System.out.println("Даны два угла треугольника (в градусах). Определить, существует ли такой треугольник," +
-                " и если да, то будет ли он прямоугольным.");
-    }
-
-    public void triangleDoesNotExist() {
-        System.out.println("Такого треугольника не существует");
-    }
-
-    public void triangleIsRectangular() {
-        System.out.println("Треугольник прямоугольный");
-    }
-
-    public void triangleExistsNotRectangular() {
-        System.out.println("Такого треугольника не существует");
+        System.out.println("Составить программу: равны ли два числа а и b?");
     }
 }
--- Java-Training/src/main/java/by/training/task2/exercise4_mvc/ViewExercise4.java
+++ Java-Training/src/main/java/by/training/task2/exercise4_mvc/ViewExercise4.java
@@ -2,7 +2,11 @@ package by.training.task2.exercise4_mvc;
 
 public interface ViewExercise4 {
     default void showResult(boolean result) {
-        System.out.println(result);
+        if (result) {
+            System.out.println("Числа равны");
+        } else {
+            System.out.println("Числа не равны");
+        }
     }
 
     void showTask();
