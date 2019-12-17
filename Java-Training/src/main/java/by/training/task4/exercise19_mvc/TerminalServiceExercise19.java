97043dc777737e20169d8f9b2c3c0183ac7c11a2
looost <m.naumenko94@gmail.com>
2019-12-17 21:54:18 +0300

    change Task4/exercise19

--- Java-Training/src/main/java/by/training/task4/exercise19_mvc/TerminalServiceExercise19.java
+++ Java-Training/src/main/java/by/training/task4/exercise19_mvc/TerminalServiceExercise19.java
@@ -3,30 +3,29 @@ package by.training.task4.exercise19_mvc;
 import java.util.ArrayList;
 
 public class TerminalServiceExercise19 {
-    private int number;
-    private int n;
-    private ArrayList<Integer> arr;
+//    private int number;
+//    private int n;
+//    private ArrayList<Integer> arr;
 
-    TerminalServiceExercise19(int number) {
-        this.number = number;
-        this.n = howManyNumber(this.number);
-        this.arr = numberArray();
-    }
+//    TerminalServiceExercise19(int number) {
+//        this.number = number;
+//        this.n = howManyNumber(this.number);
+//        this.arr = numberArray();
+//    }
 
-    private int getNumber() {
-        return number;
-    }
+//    private int getNumber() {
+//        return this.number;
+//    }
 
-    private ArrayList<Integer> numberArray() {
+    private ArrayList<Integer> numberArray(int number, int length) {
         ArrayList<Integer> arr = new ArrayList<>();
-        for (int i = n; n != 0; n--) {
-            arr.add((number / (int) Math.pow(10, n - 1)) % 10);
+        for (int i = length; length != 0; length--) {
+            arr.add((number / (int) Math.pow(10, length - 1)) % 10);
         }
         return arr;
     }
 
-    private boolean tolkoNechetnie() {
-        System.out.println(arr);
+    private boolean tolkoNechetnie(ArrayList<Integer> arr) {
         for (Integer num : arr
         ) {
             if (num % 2 == 0) {
@@ -45,7 +44,7 @@ public class TerminalServiceExercise19 {
         return count;
     }
 
-    private int howManyChetni() {
+    private int howManyChetni(ArrayList<Integer> arr) {
         System.out.println(arr);
         int count = 0;
         for (Integer num : arr
@@ -57,15 +56,38 @@ public class TerminalServiceExercise19 {
         return count;
     }
 
+    private int getSum(int f, int s) {
+        return f + s;
+    }
+
 
     public static void main(String[] args) {
-        TerminalServiceExercise19 first_number = new TerminalServiceExercise19(264);
-        TerminalServiceExercise19 second_number = new TerminalServiceExercise19(323);
-        System.out.println(first_number.tolkoNechetnie());
-        System.out.println(second_number.tolkoNechetnie());
-        TerminalServiceExercise19 summa = new TerminalServiceExercise19(first_number.getNumber() + second_number.getNumber());
-        System.out.println(summa.getNumber());
-        System.out.println(summa.howManyChetni());
-        System.out.println(summa.tolkoNechetnie());
+        TerminalServiceExercise19 numbersService = new TerminalServiceExercise19();
+
+        int firstNumber = 5313;
+        int secondNumber = 3235;
+        System.out.println("first: " + firstNumber + ", second: " + secondNumber);
+
+        int lengthFirst = numbersService.howManyNumber(firstNumber);
+        int lengthSecond = numbersService.howManyNumber(secondNumber);
+
+        ArrayList<Integer> arrFirstNumbers = numbersService.numberArray(firstNumber, lengthFirst);
+        ArrayList<Integer> arrSecondNumbers = numbersService.numberArray(secondNumber, lengthSecond);
+
+        System.out.println("Are only even numbers: " + numbersService.tolkoNechetnie(arrFirstNumbers));
+        System.out.println("Are only even numbers: " + numbersService.tolkoNechetnie(arrSecondNumbers));
+
+        int sum = numbersService.getSum(firstNumber, secondNumber);
+
+        System.out.println("Sum: " + sum);
+
+        System.out.println("Count of even digits: " + numbersService.howManyChetni(numbersService.numberArray(sum, numbersService.howManyNumber(sum))));
+
+//        System.out.println(first_number.tolkoNechetnie());
+//        System.out.println(second_number.tolkoNechetnie());
+//        TerminalServiceExercise19 summa = new TerminalServiceExercise19(first_number.getNumber() + second_number.getNumber());
+//        System.out.println(summa.getNumber());
+//        System.out.println(summa.howManyChetni());
+//        System.out.println(summa.tolkoNechetnie());
     }
 }
