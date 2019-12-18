adc64e1a3b94c578e95311d06d2400ee595e5c29
looost <m.naumenko94@gmail.com>
2019-12-18 14:57:36 +0300

    added template Task5/exercise19

---
+++ Java-Training/src/main/java/by/training/task5/exercise19_mvc/TerminalServiceExercise19.java
@@ -0,0 +1,29 @@
+package by.training.task5.exercise19_mvc;
+
+public class TerminalServiceExercise19 {
+    public static void main(String[] args) {
+        int[] arr = {3, 6, 1, 6, 3, 7, 6, 3, 6};
+        System.out.println(getResult(arr));
+    }
+
+    static int getResult(int[] arr) {
+        int countMax = 1;
+        int maxCount = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            int countCurrent = 1;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    countCurrent++;
+                    if (countCurrent > countMax) {
+                        countMax = countCurrent;
+                        maxCount = arr[i];
+                    } else if (countCurrent == countMax && arr[i] < maxCount) {
+                        maxCount = arr[i];
+                    }
+                }
+            }
+        }
+        return maxCount;
+    }
+
+}
